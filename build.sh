a4d5ee07eb67a2f23017813a2ce488b92ced73ba
mikerb <mikerb@9b418c94-3da3-478a-8d1d-45dcf0f2e76f>
2011-09-08 16:43:36 +0000

    git-svn-id: https://oceanai.mit.edu/svn/moos-ivp-extend/trunk@49 9b418c94-3da3-478a-8d1d-45dcf0f2e76f

--- build.sh
+++ build.sh
@@ -1,25 +1,89 @@
-#!/bin/sh
-##############################################################################
-#
-##############################################################################
+#!/bin/bash
+
+HELP="no"
+CLEAN="no"
+
+#-------------------------------------------------------------------
+# Define some terminal colors for convenience
+#-------------------------------------------------------------------
+
+txtrst=$(tput setaf 0)  # Reset
+txtred=$(tput setaf 1)  # Red
+txtgrn=$(tput setaf 2)  # Green
+txtylw=$(tput setaf 3)  # Yellow
+txtblu=$(tput setaf 4)  # Blue
+txtpur=$(tput setaf 5)  # Purple
+txtcyn=$(tput setaf 6)  # Cyan
+txtwht=$(tput setaf 7)  # White
+
+
+#-------------------------------------------------------------------
+#  Part 1: Check for and handle command-line arguments
+#-------------------------------------------------------------------
+for ARGI; do
+    if [ "${ARGI}" = "--help" ] ; then
+	HELP="yes"
+    fi
+    if [ "${ARGI}" = "-h" ] ; then
+	HELP="yes"
+    fi
+    if [ "${ARGI}" = "--clean" ] ; then
+	CLEAN="yes"
+    fi
+    if [ "${ARGI}" = "clean" ] ; then
+	CLEAN="yes"
+    fi
+done
+
+#-------------------------------------------------------------------
+#  Part 2: Handle the request for help
+#-------------------------------------------------------------------
+
+if [ "${HELP}" = "yes" ]; then
+    printf "${txtblu}%s [SWITCHES] ${txtrst}    \n" $0
+    printf "Switches:                           \n" 
+    printf "  --clean, clean                    \n" 
+    printf "  --help, -h                        \n" 
+    printf "Notes:                              \n"
+    printf " (1) All other command line args will be passed as args    \n"
+    printf "     to \"make\" when it is eventually invoked.            \n"
+    printf " (2) For example -k will continue making when/if a failure \n"
+    printf "     is encountered in building one of the subdirectories. \n"
+    printf " (3) For example -j2 will utilize a 2nd core in the build  \n"
+    printf "     if your machine has two cores. -j4 etc for quad core. \n"
+    exit 0;
+fi
+
+#-------------------------------------------------------------------
+#  Part 3: Invoke the call to make in the build directory
+#-------------------------------------------------------------------
 
 PWD=`pwd`
 BUILD_DIR=./build
 
 cd ${BUILD_DIR}
-rm -rf CMakeFiles CMakeCache.txt
 cmake ..
-make
+make $@
+
+
+#-------------------------------------------------------------------
+#  Part 4: Handle a request to for make clean. Most cleaning handled
+#          by the invocation of make clean, but some extras are done.
+#-------------------------------------------------------------------
+
+if [ "${CLEAN}" = "yes" ]; then
+    rm -rf CMakeCache.txt CMakeFiles/ src/ Makefile cmake_install.cmake    
+    exit 0;
+fi
+
+#-------------------------------------------------------------------
+#  Part 5: Go back to where we started from
+#-------------------------------------------------------------------
+
 cd ${PWD}
 
 
-
-
-
-
-
-
-##############################################################################
-#                          END of build-ivp-extend.sh
-##############################################################################
+############################################################
+#                   END of build.sh
+############################################################
 

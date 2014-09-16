d9623d4a1e5577ddc9267f24c9cc73a39139d402
Mohamed Saad IBN SEDDIK <ms.ibnseddik@gmail.com>
2014-09-16 10:33:28 +0200

    Adds iCamera

---
+++ src/iCamera/Camera_Tests.cpp
@@ -0,0 +1,26 @@
+/**
+ * \file Camera_Tests.cpp
+ * \brief Tests de l'application pCamera
+ * \author Team CISSAU - Veni Vidi Vici (ENSTA Bretagne)
+ * \version 0.1
+ * \date Jun 7th 2013
+ *
+ * Tests unitaires à valider pour vérifier le bon fonctionnement 
+ * de l'application MOOS
+ *
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "Camera_Tests.h"
+
+/**
+ * \fn
+ * \brief Procedure: launchTestsAndExitIfOk
+ */
+
+void launchTestsAndExitIfOk()
+{
+	Tests sessionDeTests("pCamera");
+	sessionDeTests.afficherConclusionTests();
+}
---
+++ src/iCamera/Camera_Tests.h
@@ -0,0 +1,24 @@
+/**
+ * \file Camera_Tests.h
+ * \brief Tests de l'application pCamera
+ * \author Team CISSAU - Veni Vidi Vici (ENSTA Bretagne)
+ * \version 0.1
+ * \date Jun 7th 2013
+ *
+ * Tests unitaires à valider pour vérifier le bon fonctionnement 
+ * de l'application MOOS
+ *
+ */
+
+#ifndef Camera_TESTS_HEADER
+#define Camera_TESTS_HEADER
+
+#include "../common/constantes.h"
+#include "../common/Tests.h"
+
+using namespace std;
+
+void launchTestsAndExitIfOk();
+
+#endif
+

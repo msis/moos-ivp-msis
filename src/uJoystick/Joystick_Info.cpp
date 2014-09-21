e0fd6a7bfc27d09acf05e74f52c60ff04bf4a6c7
Simon Rohou <simon.rohou@gmail.com>
2014-09-21 13:14:23 +0200

    Adding uJoystick

---
+++ src/uJoystick/Joystick_Info.cpp
@@ -0,0 +1,129 @@
+/**
+ * \file Joystick_Info.cpp
+ * \brief Informations sur l'application pJoystick
+ * \author Team CISSAU - Veni Vidi Vici (ENSTA Bretagne)
+ * \version 0.1
+ * \date Jun 7th 2013
+ *
+ * Fonctions d'aide et de présentation de l'application MOOS
+ *
+ */
+
+#include <cstdlib>
+#include <iostream>
+#include "Joystick_Info.h"
+#include "ColorParse.h"
+#include "ReleaseInfo.h"
+
+using namespace std;
+
+/**
+ * \fn
+ * \brief Procedure: showSynopsis
+ */
+
+void showSynopsis()
+{
+  blk("SYNOPSIS:                                                       ");
+  blk("------------------------------------                            ");
+  blk("  The pJoystick application is used for               ");
+  blk("                                                                ");
+  blk("                                                                ");
+  blk("                                                                ");
+  blk("                                                                ");
+}
+
+/**
+ * \fn
+ * \brief Procedure: showHelpAndExit
+ */
+
+void showHelpAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("Usage: uJoystick file.moos [OPTIONS]                   ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  showSynopsis();
+  blk("                                                                ");
+  blk("Options:                                                        ");
+  mag("  --alias","=<ProcessName>                                      ");
+  blk("      Launch pJoystick with the given process name         ");
+  blk("      rather than pJoystick.                           ");
+  mag("  --example, -e                                                 ");
+  blk("      Display example MOOS configuration block.                 ");
+  mag("  --help, -h                                                    ");
+  blk("      Display this help message.                                ");
+  mag("  --interface, -i                                               ");
+  blk("      Display MOOS publications and subscriptions.              ");
+  mag("  --version,-v                                                  ");
+  blk("      Display the release version of pJoystick.        ");
+  blk("                                                                ");
+  blk("Note: If argv[2] does not otherwise match a known option,       ");
+  blk("      then it will be interpreted as a run alias. This is       ");
+  blk("      to support pAntler launching conventions.                 ");
+  blk("                                                                ");
+  exit(0);
+}
+
+/**
+ * \fn
+ * \brief Procedure: showExampleConfigAndExit
+ */
+
+void showExampleConfigAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("pJoystick Example MOOS Configuration                   ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  blk("ProcessConfig = pJoystick                              ");
+  blk("{                                                               ");
+  blk("  AppTick   = 4                                                 ");
+  blk("  CommsTick = 4                                                 ");
+  blk("                                                                ");
+  blk("}                                                               ");
+  blk("                                                                ");
+  exit(0);
+}
+
+
+/**
+ * \fn
+ * \brief Procedure: showInterfaceAndExit
+ */
+
+void showInterfaceAndExit()
+{
+  blk("                                                                ");
+  blu("=============================================================== ");
+  blu("pJoystick INTERFACE                                    ");
+  blu("=============================================================== ");
+  blk("                                                                ");
+  showSynopsis();
+  blk("                                                                ");
+  blk("SUBSCRIPTIONS:                                                  ");
+  blk("------------------------------------                            ");
+  blk("  NODE_MESSAGE = src_node=alpha,dest_node=bravo,var_name=FOO,   ");
+  blk("                 string_val=BAR                                 ");
+  blk("                                                                ");
+  blk("PUBLICATIONS:                                                   ");
+  blk("------------------------------------                            ");
+  blk("  Publications are determined by the node message content.      ");
+  blk("                                                                ");
+  exit(0);
+}
+
+/**
+ * \fn
+ * \brief Procedure: showReleaseInfoAndExit
+ */
+
+void showReleaseInfoAndExit()
+{
+  showReleaseInfo("pJoystick", "gpl");
+  exit(0);
+}
+
